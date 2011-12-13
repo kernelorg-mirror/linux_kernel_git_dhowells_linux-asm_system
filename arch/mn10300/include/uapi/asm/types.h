549f023eea354d0d29a9947794e58ddaf873b67b
David Howells <dhowells@redhat.com>
2011-12-13 15:43:37 +0000

    UAPI: (Scripted) Disintegrate arch/mn10300/include/asm
    Signed-off-by: David Howells <dhowells@redhat.com>

--- arch/mn10300/include/uapi/asm/types.h
+++ arch/mn10300/include/uapi/asm/types.h
@@ -8,12 +8,19 @@
  * as published by the Free Software Foundation; either version
  * 2 of the Licence, or (at your option) any later version.
  */
-#ifndef _ASM_TYPES_H
-#define _ASM_TYPES_H
+#ifndef _UAPI_ASM_TYPES_H
+#define _UAPI_ASM_TYPES_H
 
-#include <uapi/asm/types.h>
+#include <asm-generic/int-ll64.h>
 
+#ifndef __ASSEMBLY__
 
-#define BITS_PER_LONG 32
+typedef unsigned short umode_t;
 
-#endif /* _ASM_TYPES_H */
+#endif /* __ASSEMBLY__ */
+
+/*
+ * These aren't exported outside the kernel to avoid name space clashes
+ */
+
+#endif /* _UAPI_ASM_TYPES_H */
