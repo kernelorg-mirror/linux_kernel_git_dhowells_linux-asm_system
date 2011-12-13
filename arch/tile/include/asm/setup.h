2f67fbfe4b586af32de7eb9b1951c0494fdf104e
David Howells <dhowells@redhat.com>
2011-12-13 15:44:06 +0000

    UAPI: (Scripted) Disintegrate arch/tile/include/asm
    Signed-off-by: David Howells <dhowells@redhat.com>

--- arch/tile/include/asm/setup.h
+++ arch/tile/include/asm/setup.h
@@ -11,16 +11,13 @@
  *   NON INFRINGEMENT.  See the GNU General Public License for
  *   more details.
  */
-
 #ifndef _ASM_TILE_SETUP_H
 #define _ASM_TILE_SETUP_H
 
-#define COMMAND_LINE_SIZE	2048
-
-#ifdef __KERNEL__
 
 #include <linux/pfn.h>
 #include <linux/init.h>
+#include <uapi/asm/setup.h>
 
 /*
  * Reserved space for vmalloc and iomap - defined in asm/page.h
@@ -30,7 +27,5 @@
 void early_panic(const char *fmt, ...);
 void warn_early_printk(void);
 void __init disable_early_printk(void);
-
-#endif /* __KERNEL__ */
 
 #endif /* _ASM_TILE_SETUP_H */
