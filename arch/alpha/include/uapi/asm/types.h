479a6d4b15464aa8571f423ff8445baceb57d0d2
David Howells <dhowells@redhat.com>
2011-12-13 15:42:56 +0000

    UAPI: (Scripted) Disintegrate arch/alpha/include/asm
    Signed-off-by: David Howells <dhowells@redhat.com>

---
+++ arch/alpha/include/uapi/asm/types.h
@@ -0,0 +1,21 @@
+#ifndef _UAPI_ALPHA_TYPES_H
+#define _UAPI_ALPHA_TYPES_H
+
+/*
+ * This file is never included by application software unless
+ * explicitly requested (e.g., via linux/types.h) in which case the
+ * application is Linux specific so (user-) name space pollution is
+ * not a major issue.  However, for interoperability, libraries still
+ * need to be careful to avoid a name clashes.
+ */
+
+#ifndef __KERNEL__
+#include <asm-generic/int-l64.h>
+#endif
+
+#ifndef __ASSEMBLY__
+
+typedef unsigned int umode_t;
+
+#endif /* __ASSEMBLY__ */
+#endif /* _UAPI_ALPHA_TYPES_H */
