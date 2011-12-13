b3ff6c7d2c1f99bd6669e8587efa5a60427d3cb8
David Howells <dhowells@redhat.com>
2011-12-13 15:44:02 +0000

    UAPI: (Scripted) Disintegrate arch/sparc/include/asm
    Signed-off-by: David Howells <dhowells@redhat.com>

--- arch/sparc/include/asm/setup.h
+++ arch/sparc/include/asm/setup.h
@@ -1,17 +1,11 @@
 /*
  *	Just a place holder. 
  */
-
 #ifndef _SPARC_SETUP_H
 #define _SPARC_SETUP_H
 
-#if defined(__sparc__) && defined(__arch64__)
-# define COMMAND_LINE_SIZE 2048
-#else
-# define COMMAND_LINE_SIZE 256
-#endif
+#include <uapi/asm/setup.h>
 
-#ifdef __KERNEL__
 
 #ifdef CONFIG_SPARC32
 /* The CPU that was used for booting
@@ -20,7 +14,5 @@
 extern unsigned char boot_cpu_id;
 extern unsigned char boot_cpu_id4;
 #endif
-
-#endif /* __KERNEL__ */
 
 #endif /* _SPARC_SETUP_H */
