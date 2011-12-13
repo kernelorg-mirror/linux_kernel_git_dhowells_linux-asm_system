5265313b07b40332b0f40120f2bc277c691507aa
David Howells <dhowells@redhat.com>
2011-12-13 15:43:58 +0000

    UAPI: (Scripted) Disintegrate arch/sh/include/asm
    Signed-off-by: David Howells <dhowells@redhat.com>

--- arch/sh/include/asm/unistd.h
+++ arch/sh/include/asm/unistd.h
@@ -1,13 +1,5 @@
-#ifdef __KERNEL__
 # ifdef CONFIG_SUPERH32
 #  include <asm/unistd_32.h>
 # else
 #  include <asm/unistd_64.h>
 # endif
-#else
-# ifdef __SH5__
-#  include <asm/unistd_64.h>
-# else
-#  include <asm/unistd_32.h>
-# endif
-#endif
